e00d16f174d3378154aee72bebcf8f27bb7a6961
GeovaniSS <geosilvasousa@gmail.com>
2022-05-07 00:19:15 -0300

    [feat]: fetch random advice api and load elements in page

--- advice-generator/assets/js/script.js
+++ advice-generator/assets/js/script.js
@@ -1,11 +1,20 @@
-const url = 'https://api.adviceslip.com/advice'
+const adviceDice = document.querySelector('.advice-dice')
 
-fetch(url)
-  .then(response => response.json())
-  .then(randomAdvice => loadAdvice(randomAdvice))
-  .catch(error => console.log(error))
+const requestRandomAdvice = async() => {
+  try {
+    const url = 'https://api.adviceslip.com/advice'
+    const response = await fetch(url)
 
-const loadAdvice = randomAdvice => {
+    if(!response.ok) throw new Error(`${response.status}: ${response.statusText}`)
+
+    const randomAdvice = await response.json()
+    loadRandomAdvice(randomAdvice)
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+const loadRandomAdvice = randomAdvice => {
   const { slip: { advice, id } } = randomAdvice
 
   const adviceId = document.querySelector('.advice-id')
@@ -14,3 +23,5 @@ const loadAdvice = randomAdvice => {
   const adviceText = document.querySelector('.advice-text')
   adviceText.innerText = advice
 }
+
+adviceDice.addEventListener('click', requestRandomAdvice)
